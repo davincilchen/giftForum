60e0f487b63e5abbbb59bb09d9957437add7bd4f
YH <doki.chen@iconic-gaming.com>
2020-11-30 19:30:02 +0800

    update models

---
+++ models/transaction.go
@@ -0,0 +1,9 @@
+package models
+
+type Transaction struct {
+	ID    int
+	UserID int
+	Point  int
+	DesUserID int
+}
+
--- models/user.go
+++ models/user.go
@@ -3,6 +3,9 @@ package models
 type User struct {
 	ID    int
 	Email string
+	Password string
+	RxPoint int
+	TxPoint int
 }
 
 type LoginUser struct {
